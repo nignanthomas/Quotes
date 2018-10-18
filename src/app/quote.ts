5db0838e46ddaf9f698beb5bf0d41467e93a22d3
nignanthomas <nignanthomas@gmail.com>
2018-10-18 19:12:42 +0300

    create/define the class Quote in quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+
+  public showDetails:boolean;
+  constructor (
+    public id:number,
+    public textQuote:string,
+    public author:string,
+    public upVote:number,
+    public downVote:number,
+    public username:string) {
+
+      this.showDetails = false;
+  }
+
 }
