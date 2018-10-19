d62f1cc2639cb8c0da03290547c21a767f814a88
nignanthomas <nignanthomas@gmail.com>
2018-10-19 16:44:24 +0300

    create functions to emit events to parent component in quote-details.component.ts

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -9,6 +9,20 @@ import { Quote } from '../quote';
 export class QuoteDetailsComponent implements OnInit {
 
   @Input() quote:Quote;
+
+  @Output() isDelete = new EventEmitter<boolean>;
+  @Output() isPlus = new EventEmitter<boolean>;
+  @Output() isMinus = new EventEmitter<boolean>;
+
+  quoteDelete(delete:boolean) {
+    this.isDelete.emit(delete);
+  }
+  votePlus(plus:boolean) {
+    this.isPlus.emit(plus);
+  }
+  voteMinus(minus:boolean) {
+    this.isMinus.emit(minus);
+  }
 
 
   constructor() { }
