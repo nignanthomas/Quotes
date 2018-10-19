8a9e9087d3a4fc202a86cd0acafd2e5000f872d8
nignanthomas <nignanthomas@gmail.com>
2018-10-19 10:48:27 +0300

    import class Quote to quote.component.ts and create an array with two quotes; create the layout template to display quotes in quote.component.html

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1, "quote", "author", 0, 0, "username"),
+    new Quote(2, "quote", "author", 0, 0, "username"),
+  ]
 
   constructor() { }
 
